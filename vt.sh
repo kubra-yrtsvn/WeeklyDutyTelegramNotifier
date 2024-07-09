b4c036022af9356ef310cd6eb6601cc6802ba14b
kubra-yrtsvn <128634402+kubra-yrtsvn@users.noreply.github.com>
2024-07-09 10:56:34 +0300

    chat ID ve token değiştirildi

--- vt.sh
+++ vt.sh
@@ -10,10 +10,10 @@ gorevli=$(psql -U kubra -d postgres -t -c "SELECT name FROM mytable  WHERE date 
 if [ -z "$gorevli" ]; then
   echo "Eşleşen görevli bulunamadı: $tarih"
   # Telegram mesajı gönderme komutu 
-  curl -s -X POST https://api.telegram.org/bot7207999152:AAGiJRGHJDx6lbzuOQMK9Og6FLPeeUAcVPk/sendMessage -d chat_id=5969240980 -d text="Eşleşen görevli bulunamadı: $tarih"
+  curl -s -X POST https://api.telegram.org/bot<BOT_TOKEN>/sendMessage -d chat_id=<CHAT_ID> -d text="Eşleşen görevli bulunamadı: $tarih"
 else
   echo "Görevli: $gorevli"
   # Telegram mesajı gönderme komutu 
-  curl -s -X POST https://api.telegram.org/bot7207999152:AAGiJRGHJDx6lbzuOQMK9Og6FLPeeUAcVPk/sendMessage -d chat_id=5969240980 -d text="Haftanın görevlisi: $gorevli"
+  curl -s -X POST https://api.telegram.org/bot<BOT_TOKEN>/sendMessage -d chat_id=<CHAT_ID> -d text="Haftanın görevlisi: $gorevli"
 fi
 
